8c4889d38706ef39086ee329757ed38f96e8c33b
Cengiz TORU <cengiztoru@gmail.com>
2020-07-01 01:41:31 +0300

    Home Development

--- app/src/main/java/com/cengiztoru/samplenavigationexample/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/cengiztoru/samplenavigationexample/ui/fragments/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.cengiztoru.samplenavigationexample.R
 import com.cengiztoru.samplenavigationexample.data.User
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -12,6 +15,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 class HomeFragment : Fragment() {
 
     lateinit var user: User
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +33,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         message.text = "Welcome ${user.name} ${user.surname} \n ${user.email}"
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.btnLogout).setOnClickListener {
+            navController.navigate(R.id.action_homeFragment_to_loginFragment)
+        }
     }
 
 }
