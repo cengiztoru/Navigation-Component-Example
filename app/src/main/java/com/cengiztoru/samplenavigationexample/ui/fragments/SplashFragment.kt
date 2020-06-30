d973d1434c66dc9bc030660bbbc4061efe77a7d7
Cengiz TORU <cengiztoru@gmail.com>
2020-06-30 23:29:32 +0300

    Showing Login Screen Development

--- app/src/main/java/com/cengiztoru/samplenavigationexample/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/cengiztoru/samplenavigationexample/ui/fragments/SplashFragment.kt
@@ -5,9 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.cengiztoru.samplenavigationexample.R
+import kotlin.random.Random
 
 class SplashFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    //initalize Navigation Controller
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        checkUserThanGoNextScreen()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +27,23 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun checkUserThanGoNextScreen() {
+        when (isUserLogged()) {
+            false -> {
+                navController.navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+            true -> {
+                //todo
+            }
+        }
+    }
+
+    //if result of mode random number to 2 is 0 funtion will return true else return false
+    private fun isUserLogged(): Boolean {
+        //TODO YOU CAN CUSTOMIZE HERE FOR YOU
+        return Random.nextInt() % 2 == 0
     }
 
 }
